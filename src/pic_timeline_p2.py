752ecb93c981e47302164c52e9dab657127e648a
popowow <comrademisc@gmail.com>
2012-12-28 23:07:12 -0800

    Added license

--- src/pic_timeline_p2.py
+++ src/pic_timeline_p2.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# Copyright (c) 2012-2013 Kyle Kawamura
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 # Backstory: Nowadays, everything is a camera.  When I get back from a trip
 # I'll have pictures from several cameras as well as items that I scan in
 # manually.  Here's the problem: I like being able to look at the photos
@@ -25,6 +45,9 @@
 # and the output directory and then go!  You now have all your photos    
 # reordered sequentially according to filename.  The files are not
 # changed at all except for the name.
+#
+# NOTES:
+# - EXIF date-taken is used if found.  Otherwise modified-time.
 #
 # HINTS:
 #  - Add sources that will need extensive datetime overrides first.
@@ -142,7 +165,7 @@ class PicTimelineApp(Frame):
         filename: Name of original image name to process
         data: Reference to source data object.  Used to get timeshift/color info.
         _dt: datetime information from file.  "dt" property can shift this
-             with source's timeshift info (if any) or completely overriden
+             with source's timeshift info (if any) or completely overridden
              by _dt_override.
         """
         def __init__(self, id, filename, data, dt):
